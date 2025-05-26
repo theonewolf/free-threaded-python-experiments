127e09b95ea4dca30861674ef6b0792fea83e066
Wolfgang Richter <wolfgang.richter@gmail.com>
2025-05-26 12:03:25 +0000

    run.sh: run both server types, and only up to 4 client threads on M1

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 ./py313stable/bin/python controller.py \
-  --server-type multiprocess \
+  --server-type threaded multiprocess \
   --server-threads 1 2 3 4 \
-  --max-client-threads 8 \
+  --max-client-threads 4 \
   --benchmark-duration 15 \
   --mode compute
